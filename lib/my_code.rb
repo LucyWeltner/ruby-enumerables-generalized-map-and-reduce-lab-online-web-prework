84bae9f726b30785a62109279296b0cd6c623a03
Lucy Weltner <lucyiweltner@gmail.com>
2019-11-05 00:58:31 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -10,7 +10,7 @@ def map(array)
   new_array
 end 
 
-def reduce(array, startingpoint = nil)
+def reduce(array, startingpoint = 0)
   i = 0 
   if startingpoint.class == Integer
     while i < array.length do
